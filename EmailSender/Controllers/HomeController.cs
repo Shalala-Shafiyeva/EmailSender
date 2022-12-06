47be7b47d52d3e2d8727aafedecb0beb54d38f5d
Shalala Shafiyeva <shalala.shafiyeva23@gmail.com>
2022-12-06 12:05:10 +0400

    Add project files.

---
+++ EmailSender/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace EmailSender.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("shalala.shafiyeva23@gmail.com"));
+            email.To.Add(MailboxAddress.Parse("shalala.shafiyeva23@gmail.com"));
+            email.Subject = "Hello Message";
+            email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.gmail.com", 587);
+            smtp.Authenticate("shalala.shafiyeva23@gmail.com", "julnvkdbnafxteqx");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+        }
+    }
+}
